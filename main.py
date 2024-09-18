e221010efbc8808527ae3f280a1d9f8c1270d5d6
PHGMorgan <morgan.hutchens777@gmail.com>
2024-09-18 01:12:36 -0700

    updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        word_count = file_contents.split()
-        print(f"This book contains: {len(word_count)} words.")
+    book_filepath = "books/frankenstein.txt"
+    book_contents = book_read(book_filepath)
+    split_book = list_of_words(book_contents)
+    word_count = num_of_words(split_book)
+    print(f"This book contains: {word_count} words.")
+
+def book_read(book):
+    with open(book) as f:
+        contents = f.read()
+    return contents
+
+def list_of_words(words):
+    return words.split()
+
+def num_of_words(words):
+    return len(words)
 
 if __name__ == "__main__":
     main()
